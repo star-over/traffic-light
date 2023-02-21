ef9d1eeef2410f2e44ab898a66e86fb49a00e763
FFire <8497715@gmail.com>
2023-02-21 20:48:05 +0500

    fix: remove useEffect

--- src/components/TrafficLight.jsx
+++ src/components/TrafficLight.jsx
@@ -3,58 +3,11 @@
 /* eslint-disable import/no-unresolved */
 import { trafficManagerMachine } from "machines/trafficManagerMachine";
 import { useMachine } from "@xstate/react";
-import { useEffect, useState } from "react";
 import { Light } from "./Light";
 import { ManageControl } from "./ManageControl";
 
 export function TrafficLight() {
   const [manager, sendToManager] = useMachine(trafficManagerMachine);
-  const [buttonStates, setButtonsState] = useState({
-    turnOff: {
-      isNext: false,
-      isSelected: false,
-      caption: "Turn off",
-      stateName: "turnedOff",
-      eventName: "TO_TURNED_OFF",
-      handle: () => sendToManager("TO_TURNED_OFF"),
-    },
-    standBy: {
-      isNext: false,
-      isSelected: false,
-      caption: "Stand by",
-      stateName: "standby",
-      eventName: "TO_STANDBY",
-      handle: () => sendToManager("TO_STANDBY"),
-    },
-    run: {
-      isNext: false,
-      isSelected: false,
-      caption: "Run",
-      stateName: "running",
-      eventName: "TO_RUN",
-      handle: () => sendToManager("TO_RUN"),
-    },
-  });
-
-  useEffect(() => {
-    setButtonsState((prev) => ({
-      turnOff: {
-        ...prev.turnOff,
-        isNext: manager.nextEvents.includes("TO_TURNED_OFF"),
-        isSelected: manager.matches("turnedOff"),
-      },
-      standBy: {
-        ...prev.standBy,
-        isNext: manager.nextEvents.includes("TO_STANDBY"),
-        isSelected: manager.matches("standby"),
-      },
-      run: {
-        ...prev.run,
-        isNext: manager.nextEvents.includes("TO_RUN"),
-        isSelected: manager.matches("running"),
-      },
-    }));
-  }, [manager]);
 
   return (
     <div className="container flex flex-col items-center justify-center">
