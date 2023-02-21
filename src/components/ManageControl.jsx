993418d8ee076a9720171453cd80ae968b3dd641
FFire <8497715@gmail.com>
2023-02-21 21:20:12 +0500

    fix: refactoring

--- src/components/ManageControl.jsx
+++ src/components/ManageControl.jsx
@@ -1,81 +1,33 @@
 "use client";
 
 /* eslint-disable import/no-unresolved */
-import { useEffect, useState } from "react";
 import { ManageButton } from "./ManageButton";
 
 export function ManageControl({ manager, sendToManager }) {
-  const [buttonStates, setButtonsState] = useState({
-    turnOff: {
-      isNext: false,
-      isSelected: false,
-      caption: "Turn off",
-      stateName: "turnedOff",
-      eventName: "TO_TURNED_OFF",
-      handle: () => sendToManager("TO_TURNED_OFF"),
-    },
-    standBy: {
-      isNext: false,
-      isSelected: false,
-      caption: "Stand by",
-      stateName: "standby",
-      eventName: "TO_STANDBY",
-      handle: () => sendToManager("TO_STANDBY"),
-    },
-    run: {
-      isNext: false,
-      isSelected: false,
-      caption: "Run",
-      stateName: "running",
-      eventName: "TO_RUN",
-      handle: () => sendToManager("TO_RUN"),
-    },
-  });
-
-  useEffect(() => {
-    setButtonsState((prev) => ({
-      turnOff: {
-        ...prev.turnOff,
-        isNext: manager.nextEvents.includes("TO_TURNED_OFF"),
-        isSelected: manager.matches("turnedOff"),
-      },
-      standBy: {
-        ...prev.standBy,
-        isNext: manager.nextEvents.includes("TO_STANDBY"),
-        isSelected: manager.matches("standby"),
-      },
-      run: {
-        ...prev.run,
-        isNext: manager.nextEvents.includes("TO_RUN"),
-        isSelected: manager.matches("running"),
-      },
-    }));
-  }, [manager]);
-
   return (
     <div className="">
 
       <ManageButton
         className="rounded-l-full"
-        enabled={buttonStates.turnOff.isNext}
-        selected={buttonStates.turnOff.isSelected}
-        onClick={buttonStates.turnOff.handle}
-      >{buttonStates.turnOff.caption}
+        enabled={manager.nextEvents.includes("TO_TURNED_OFF")}
+        selected={manager.matches("turnedOff")}
+        onClick={() => sendToManager("TO_TURNED_OFF")}
+      >Turn off
       </ManageButton>
 
       <ManageButton
-        enabled={buttonStates.standBy.isNext}
-        selected={buttonStates.standBy.isSelected}
-        onClick={buttonStates.standBy.handle}
-      >{buttonStates.standBy.caption}
+        enabled={manager.nextEvents.includes("TO_STANDBY")}
+        selected={manager.matches("standby")}
+        onClick={() => sendToManager("TO_STANDBY")}
+      >Stand by
       </ManageButton>
 
       <ManageButton
         className="rounded-r-full"
-        enabled={buttonStates.run.isNext}
-        selected={buttonStates.run.isSelected}
-        onClick={buttonStates.run.handle}
-      >{buttonStates.run.caption}
+        enabled={manager.nextEvents.includes("TO_RUN")}
+        selected={manager.matches("running")}
+        onClick={() => sendToManager("TO_RUN")}
+      >Run
       </ManageButton>
 
     </div>
